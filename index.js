7a713c8d1d6e3eaf8f8b01449d63db0b23987473
Ricardo Munhóz Ribeiro <munhozribeiroricardo@gmail.com>
2024-08-13 15:01:15 -0300

    adicionei nome e sobrenome

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ app.get('/', function(req, res){
 })
 
 app.get('/Ricardo', function(req, res){
-    res.send("Olá, Ricardo!")
+    res.send("Ricardo!")
 })
 
+app.get("/:x", function(req,res){
+    const mensagem = "Oi " + req.params.x;
+    res.send(mensagem)
+})
 
-
+app.get('/:nome/:sobrenome', function(req, res){
+    const mensagem = 'Oi ' + req.params.nome + ' ' + req.params.sobrenome;
+    res.send(mensagem)
+});
 
 
 app.listen(3000);
